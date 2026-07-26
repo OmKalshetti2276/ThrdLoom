90227afd50ea4e8a735344273992b87ee5ae6cf5
Om Kalshetti <omkalshetti22@gmail.com>
2026-07-26 20:15:38 +0530

    Refactor embedding service to use lazy model initialization

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,11 +1,18 @@
 from sentence_transformers import SentenceTransformer
 
-embedding_model= SentenceTransformer("all-MiniLM-L6-v2")
+_embedding_model = None
 
-def generate_embedding(text:str):
 
-    result= embedding_model.encode(text)
+def get_embedding_model():
+    global _embedding_model
 
+    if _embedding_model is None:
+        _embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    return _embedding_model
+
+
+def generate_embedding(text: str):
+    model = get_embedding_model()
+    result = model.encode(text)
     return result.tolist()
-
-
